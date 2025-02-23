7995924ecbb04edfc320cbd46af49055c1d7e9eb
ppadjin <pavle.padjin@gmail.com>
2025-02-23 17:48:35 +0000

    renaming to flwr core

---
+++ flbs/flwr_core.py
@@ -0,0 +1,276 @@
+import random
+from pathlib import Path
+import tarfile
+from typing import Any
+from logging import INFO, DEBUG
+from collections import defaultdict, OrderedDict
+from collections.abc import Sequence, Callable
+import numbers
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import Module
+from torch.utils.data import DataLoader, Dataset
+from enum import IntEnum
+import flwr
+from flwr.server import History, ServerConfig
+from flwr.server.strategy import FedAvgM as FedAvg, Strategy
+from flwr.common import log, NDArrays, Scalar, Parameters, ndarrays_to_parameters
+from flwr.client.client import Client
+
+import matplotlib.pyplot as plt
+
+
+from .common.client_utils import (
+    Net,
+    load_femnist_dataset,
+    get_network_generator_cnn as get_network_generator,
+    train_femnist,
+    test_femnist,
+    save_history,
+)
+from .utils import get_git_root
+from .estimate import (
+    collect_gradients,
+    compute_noise_scale_from_gradients,
+)
+
+
+# Add new seeds here for easy autocomplete
+class Seeds(IntEnum):
+    """Seeds for reproducibility."""
+
+    DEFAULT = 1337
+
+
+def set_all_seeds():
+    np.random.seed(Seeds.DEFAULT)
+    random.seed(Seeds.DEFAULT)
+    torch.manual_seed(Seeds.DEFAULT)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_device() -> str:
+    """Get the device (cuda, mps, cpu)."""
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = "mps"
+    return device
+
+def relevant_paths():
+    home_dir = get_git_root()
+    dataset_dir: Path = home_dir / "femnist"
+    data_dir: Path = dataset_dir / "data"
+    centralized_partition: Path = dataset_dir / "client_data_mappings" / "centralized"
+    centralized_mapping: Path = dataset_dir / "client_data_mappings" / "centralized" / "0"
+    federated_partition: Path = dataset_dir / "client_data_mappings" / "fed_natural"
+    
+    paths = {
+        "dataset_dir": dataset_dir,
+        "data_dir": data_dir,
+        "centralized_partition": centralized_partition,
+        "centralized_mapping": centralized_mapping,
+        "federated_partition": federated_partition
+    }
+    return paths
+
+
+def decompress_dataset(paths: dict):
+    if not paths["dataset_dir"].exists():
+        with tarfile.open(paths["dataset_dir"] / "femnist.tar.gz", "r:gz") as tar:
+            tar.extractall(path=paths["dataset_dir"])
+        log(INFO, "Dataset extracted in %s", paths["dataset_dir"])
+
+
+def set_model_parameters(net: Module, parameters: NDArrays) -> Module:
+    """Put a set of parameters into the model object."""
+    weights = parameters
+    params_dict = zip(net.state_dict().keys(), weights, strict=False)
+    state_dict = OrderedDict({k: torch.from_numpy(np.copy(v)) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
+    return net
+
+
+def get_model_parameters(net: Module) -> NDArrays:
+    """Get the current model parameters as NDArrays."""
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+class FlowerRayClient(flwr.client.NumPyClient):
+    """Flower client for the FEMNIST dataset."""
+
+    def __init__(
+        self,
+        cid: int,
+        partition_dir: Path,
+        model_generator: Callable[[], Module],
+    ) -> None:
+        """Init the client with its unique id and the folder to load data from.
+
+        Parameters:
+            cid (int): Unique client id for a client used to map it to its data
+                partition
+            partition_dir (Path): The directory containing data for each
+                client/client id
+            model_generator (Callable[[], Module]): The model generator function
+        
+        """
+        self.cid = cid
+        log(INFO, "cid: %s", self.cid)
+        self.partition_dir = partition_dir
+        self.device = str(
+            torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        )
+        self.model_generator: Callable[[], Module] = model_generator
+        self.properties: dict[str, Scalar] = {"tensor_type": "numpy.ndarray"}
+
+    def set_parameters(self, parameters: NDArrays) -> Module:
+        """Load weights inside the network."""
+        net = self.model_generator()
+        return set_model_parameters(net, parameters)
+
+    def get_parameters(self, config: dict[str, Scalar]) -> NDArrays:
+        """Return weights from a given model.
+
+        If no model is passed, then a local model is created.
+        This can be used to initialise a model in the
+        server.
+        The config param is not used but is mandatory in Flower.
+
+        """
+        net = self.model_generator()
+        return get_model_parameters(net)
+
+    def fit(self, parameters: NDArrays, config: dict[str, Scalar]) -> tuple[NDArrays, int, dict]:
+        """Receive and train a model on the local client data."""
+        # Only create model right before training/testing
+        # To lower memory usage when idle
+        net = self.set_parameters(parameters)
+        net.to(self.device)
+
+        train_loader: DataLoader = self._create_data_loader(config, name="train")
+        train_loss = self._train(net, train_loader=train_loader, config=config)
+
+        # Compute gradients
+        # Collect gradients for noise scale estimation.
+        grad_vectors = collect_gradients(net, train_loader, self.device, torch.nn.CrossEntropyLoss(), 5)
+        # Compute local noise scale (Bsimple) on this client.
+        local_noise_scale = compute_noise_scale_from_gradients(grad_vectors)
+
+        return get_model_parameters(net), len(train_loader), {"train_loss": train_loss, "noise_scale": local_noise_scale}
+
+    def evaluate(self, parameters: NDArrays, config: dict[str, Scalar]) -> tuple[float, int, dict]:
+        """Receive and test a model on the local client data."""
+        net = self.set_parameters(parameters)
+        net.to(self.device)
+
+        test_loader: DataLoader = self._create_data_loader(config, name="test")
+        loss, accuracy = self._test(net, test_loader=test_loader, config=config)
+        return loss, len(test_loader), {"local_accuracy": accuracy}
+
+    def _create_data_loader(self, config: dict[str, Scalar], name: str) -> DataLoader:
+        """Create the data loader using the specified config parameters."""
+        batch_size = int(config["batch_size"])
+        num_workers = int(config["num_workers"])
+        dataset = self._load_dataset(name)
+        return DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+            drop_last=(name == "train"),
+        )
+
+    def _load_dataset(self, name: str) -> Dataset:
+        full_file: Path = self.partition_dir / str(self.cid)
+        return load_femnist_dataset(
+            mapping=full_file,
+            name=name,
+            data_dir=data_dir,
+        )
+
+    def _train(
+        self, net: Module, train_loader: DataLoader, config: dict[str, Scalar]
+    ) -> float:
+        return train_femnist(
+            net=net,
+            train_loader=train_loader,
+            epochs=int(config["epochs"]),
+            device=self.device,
+            optimizer=torch.optim.AdamW(
+                net.parameters(),
+                lr=float(config["client_learning_rate"]),
+                weight_decay=float(config["weight_decay"]),
+            ),
+            criterion=torch.nn.CrossEntropyLoss(),
+            max_batches=int(config["max_batches"]),
+        )
+
+    def _test(
+        self, net: Module, test_loader: DataLoader, config: dict[str, Scalar]
+    ) -> tuple[float, float]:
+        return test_femnist(
+            net=net,
+            test_loader=test_loader,
+            device=self.device,
+            criterion=torch.nn.CrossEntropyLoss(),
+            max_batches=int(config["max_batches"]),
+        )
+
+    def get_properties(self, config: dict[str, Scalar]) -> dict[str, Scalar]:
+        """Return properties for this client."""
+        return self.properties
+
+    def get_train_set_size(self) -> int:
+        """Return the client train set size."""
+        return len(self._load_dataset("train"))  # type: ignore[reportArgumentType]
+
+    def get_test_set_size(self) -> int:
+        """Return the client test set size."""
+        return len(self._load_dataset("test"))  # type: ignore[reportArgumentType]
+
+
+def fit_client_seeded(
+    client: FlowerRayClient,
+    params: NDArrays,
+    conf: dict[str, Any],
+    seed: Seeds = Seeds.DEFAULT,
+    **kwargs: Any,
+) -> tuple[NDArrays, int, dict]:
+    """Wrap to always seed client training."""
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    return client.fit(params, conf, **kwargs)
+
+
+def get_flower_client_generator(
+    model_generator: Callable[[], Module],
+    partition_dir: Path,
+    mapping_fn: Callable[[int], int] | None = None,
+) -> Callable[[str], FlowerRayClient]:
+    """Wrap the client instance generator.
+
+    A mapping function could be used for filtering/ordering clients.
+
+    Parameters
+    ----------
+        model_generator (Callable[[], Module]): model generator function.
+        partition_dir (Path): directory containing the partition.
+        mapping_fn (Optional[Callable[[int], int]]): function mapping sorted/filtered
+            ids to real cid.
+    """
+
+    def client_fn(cid: str) -> FlowerRayClient:
+        """Create a single client instance given the client id `cid`."""
+        return FlowerRayClient(
+            cid=mapping_fn(int(cid)) if mapping_fn is not None else int(cid),
+            partition_dir=partition_dir,
+            model_generator=model_generator,
+        )
+
+    return client_fn
